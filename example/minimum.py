1e49f39e4a117eedd16766b90c351541db111b41
Aetf <aetf@unlimitedcodeworks.xyz>
2017-01-19 23:21:05 -0500

    More ops in minimum

--- example/minimum.py
+++ example/minimum.py
@@ -19,6 +19,7 @@ with tf.attr_scope(parallelism='data'):
     net = tf.nn.conv2d(x, W2, padding='SAME', name='conv/conv2d')
 
 net = tf.reshape(net, [batch_size, 7840], name='fc/reshape')
+
 with tf.attr_scope(parallelism='model'):
     net = tf.matmul(net, W, name='fc/matmul')
 
@@ -26,39 +27,40 @@ y = tf.nn.softmax(net, name='y')
 
 y_ = tf.placeholder(tf.float32, [batch_size, 10], name='labels')
 
+# Define loss
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]), name='cross_entropy')
+
+# Define accuracy
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(correct_prediction, name='accuracy')
+
 # Apply transform
-g = graph.create(y)
+g = graph.create(symbol.Group([y, cross_entropy, accuracy]))
 print('Before')
-g = g.apply('DumpGraph')
+g._set_json_attr('dotgraph_output', '/tmp/workspace/before.dot')
+g.apply('DotGraph')
 
 print('\n\nParallelizing')
 g = g.apply('Parallelize')
 
 print('\n\nAfter')
-g = g.apply('DumpGraph')
+g._set_json_attr('dotgraph_output', '/tmp/workspace/after.dot')
 g.apply('DotGraph')
 
-## Define loss
-#cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]), name='cross_entropy')
 
-## Define accuracy
-#correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-#accuracy = tf.reduce_mean(correct_prediction, name='accuracy')
-
-
-print('\n\nVariable Initialization')
+print('\n\nVariable initialization')
 sess = tf.Session()
 sess.run(tf.initialize_all_variables())
+
+# Optimizer to train
+print('\n\nCreating optimizer')
+#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 ## get the mnist dataset
 #mnist = get_mnist(flatten=True, onehot=True)
 #batch_xs, batch_ys = mnist.train.next_batch(100)
+
 #sess.run([y, cross_entropy, accuracy], feed_dict={x: batch_xs, y_:batch_ys})
-
-## Optimizer to train
-#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-
 
 #for i in range(1000):
 #    batch_xs, batch_ys = mnist.train.next_batch(100)
